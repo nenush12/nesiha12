67925d9b888944670cf69842f4f2dd88cdc4b154
nenush12 <nesihanurselams@gmail.com>
2025-08-12 15:35:53 +0100

    Add files via upload

---
+++ peredic table.py
@@ -0,0 +1,6 @@
+atomic_number = int(input("enter atomic number :"))
+element = periodictable.elements[atomic_number]
+print("Name: , element name)
+print("symbol": , elemnt.symbol)
+print("atomic mass:" , element.mass)
+print("density:" , element.de3nsity)
